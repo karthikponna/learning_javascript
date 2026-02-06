1a96cab252c90327c59737ea9020b2de166dcd79
karthikponna <karthikponna963@gmail.com>
2026-02-06 20:01:25 +0530

    Initial commit

---
+++ basics.js
@@ -0,0 +1,106 @@
+// const is constant where that won't be chaning anytime soon 
+
+// const username = "Karthik";
+// console.log(username);
+
+
+// Booleans 
+
+// const isStudent = false;
+// const isLoggedIn = true;
+
+// console.log(isStudent);
+// console.log(isLoggedIn);
+
+
+// arrays
+
+// const fruits = ["Apple", "Mango", "Banana"];
+// console.log(fruits[2])
+
+
+// object
+
+// const person = {
+//     name: "Karthik",
+//     age: 23, 
+// };
+// console.log(person.age)
+
+
+// null
+// means there is nothing here yet
+
+// undefined
+// means this hasn't been given a value yet 
+
+
+// functions
+
+// function sayHello() {
+//     console.log("hello")
+// }
+
+// sayHello();
+
+// function sayHello(name) {
+//     console.log("hello " + name)
+// }
+
+// sayHello("Karthik");
+
+// function sayHello(name) {
+//     console.log("hello " + name.name)
+// }
+
+// sayHello({name: "Karthik"});
+
+
+// if else statements
+
+// let age = 20
+
+// if (age > 18) {
+//     console.log("your are an adult");
+// } else {
+//     console.log("you are minor");
+// }
+
+
+// set time out ( this is how asynchrous programing works)
+
+// console.log("Start");
+
+// setTimeout(() => {
+//     console.log("This runs after 2 seconds");
+// }, 2000)
+
+// console.log('end');
+
+
+
+// using arrow functions to write functions more shortly
+
+// const add = (a, b) => {
+//     return a+b;
+// };
+
+// console.log(add(4 , 5))
+
+
+
+// everything together
+
+// function introduce(person) {
+//     console.log("Hey welcome " + person.name);
+//     if (person.age >= 18) {
+//         console.log("I'm an adult");
+//     } else {
+//         console.log("You are minor");
+//     }
+// }
+
+// introduce({name: "Karthik", age: 23});
+
+
+
